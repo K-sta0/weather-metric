7fe3f7676e937e686459b89e114c1d6f9345a7be
K_sta0 <kosttyagr@gmail.com>
2026-05-09 22:55:04 +0200

    small change to dark navbar

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="min-h-screen bg-base-200">
       {/* Navigation bar */}
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-neutral text-neutral-content shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">Weathermetric</a>
         </div>
